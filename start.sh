12e2be7b2238e0341b74731e493c4299c4960161
Ilia Stepanov <ilia.stepanov@sap.com>
2024-10-02 18:45:29 +0200

    start

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          haltusbpower
+# Required-Start:    $all
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:
+# Short-Description: Halts USB power...
+### END INIT INFO
+
 java -jar ~/ytdtb/target/ytdownloadbot-0.0.1-SNAPSHOT.jar
 
