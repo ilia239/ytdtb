ebc1a9d6b2271ebf785516e0af345cc5e8dd4453
Ilia Stepanov <ilia.stepanov@sap.com>
2024-10-02 23:11:47 +0200

    start

--- src/main/java/com/ytdtb/app/Web.java
+++ src/main/java/com/ytdtb/app/Web.java
@@ -1,11 +1,17 @@
 package com.ytdtb.app;
 
 import jakarta.servlet.http.HttpServletResponse;
+import javassist.bytecode.SignatureAttribute;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,11 +22,22 @@ public class Web {
     @Value( "${data.dir}" )
     public String dataDirectory;
 
-    @RequestMapping(value = "/data/{youtube_id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/data1/{youtube_id}", method = RequestMethod.GET)
     @ResponseBody
     public FileSystemResource getData(@PathVariable("youtube_id") String youtubeId, HttpServletResponse response) {
         log.log(Level.INFO, "getData: "+ youtubeId);
         response.setContentType("video/mp4");
+        return new FileSystemResource(new File(dataDirectory + youtubeId+".mp4"));
+    }
+
+
+    @RequestMapping(value = "/data/{youtube_id}", method = RequestMethod.GET)
+    public FileSystemResource doAction(@PathVariable("youtube_id")String youtubeId, HttpServletResponse response) throws IOException {
+        String filename = youtubeId+".mp4";
+        File file = new File(dataDirectory + filename);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        response.setHeader("Content-Disposition", "attachment; filename=" + filename);
         return new FileSystemResource(new File(dataDirectory + youtubeId+".mp4"));
     }
 }
