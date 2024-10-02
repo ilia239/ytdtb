1681002a6b9bd2ed3a594b5dd4a0ebde1df37b77
Ilia Stepanov <ilia.stepanov@sap.com>
2024-10-02 15:33:35 +0200

    first commit

---
+++ src/main/java/com/ytdtb/ytdownloadbot/YtdownloadbotApplication.java
@@ -0,0 +1,13 @@
+package com.ytdtb.ytdownloadbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtdownloadbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtdownloadbotApplication.class, args);
+	}
+
+}
